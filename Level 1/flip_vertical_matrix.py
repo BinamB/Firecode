bbe8e5efe5041bf522f6d409478b295d331b112d
BinamB <44302895+BinamB@users.noreply.github.com>
2019-02-05 10:17:18 -0600

    Revert "Revert "Revert "Level 1 and Level2"""
    This reverts commit 67d993a4f66529ff7dedaf89a89711b158ba7637.

--- Level 1/flip_vertical_matrix.py
+++
@@ -1,4 +0,0 @@
-ef flip_vertical_axis(matrix):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])//2):
-            matrix[i][j], matrix[i][len(matrix[i])-1-j] = matrix[i][len(matrix[i])-1-j], matrix[i][j]
--- Level 2/bubbleSort.py
+++
@@ -1,8 +0,0 @@
-def bubble_sort(a_list):
-    for i in range(len(a_list)-1, -1, -1):
-        for j in range(i):
-            if a_list[j] > a_list[j+1]:
-                a_list[j], a_list[j+1] = a_list[j+1], a_list[j]
-    return a_list
-                
-        
